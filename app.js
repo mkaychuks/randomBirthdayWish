7f6d3f33a98d7e758b05384b575562ae3ce4a482
mkaychuks <sachufusi@gmail.com>
2021-06-24 12:35:58 -0700

    headaches  solved :rocket:

--- app.js
+++ app.js
@@ -7,7 +7,25 @@ const app = express();
 // init express middlewares for the app
 app.use(express.json());
 
+// creating a function that randomizes the quotes to be sent
+function randomize(arr) {
+  const randomReturns = arr[Math.floor(Math.random() * arr.length)];
+  return randomReturns;
+}
+
+// init the post request route that handles POST
+app.post('/', (req, res) => {
+  const { name, dob } = req.body;
+  //const wish = randomQuotes[Math.floor(Math.random() * randomQuotes.length)];
+  const newWish = {
+    name: name,
+    dob: dob,
+    wishes: randomize(randomQuotes), // utilizing the function created for the randomize
+  };
+  res.send(newWish);
+});
+
 // init express server connection
-app.listen(() => {
-  console.log('Server is listening onm port 3000...');
+app.listen(5000, () => {
+  console.log('Server is listening on port 5000...');
 });
